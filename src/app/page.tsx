7a526d41a6cfd0b3837baa7314034f311afafb03
Johnny <joaogbdesouza@gmail.com>
2024-01-07 16:33:20 -0300

    adicionando tabela de despesas na página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,94 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import ExpensesTable from "@/ui/table"
 
-export default function Home() {
+
+async function getData(mes: string, ano: string) {
+  let date: string = `${ano}-${mes}-01`
+  if(!mes || !ano){
+    date = ""
+  }
+
+  console.log(date)
+  const url: string = `http://localhost:8080/expenses?date=${date}`
+  const res = await fetch(url)
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return await res.json()
+}
+
+const geraAnosSelect = (): Array<number> => {
+  let anoInicial: number = 2020
+  const anoMaximo: number = 2070
+
+  const resultado: Array<number> = []
+
+  while (anoInicial <= anoMaximo) {
+      resultado.push(anoInicial)
+      anoInicial++
+  }
+  
+  return resultado
+}
+
+
+export default async function Home({searchParams}) {
+  console.log(searchParams)
+  const expensesResponse: any = await getData(searchParams.mes, searchParams.ano)
+  const expenses: Expense[] = expensesResponse.expenses
+  const anos = geraAnosSelect()
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+        <form>
+          <div className="row mt-3 mb-3">
+            <div className="col-md-3">
+              <label htmlFor="mes" className ="form-label">Mês</label>
+              <select className="form-control" name="mes" id="mes">
+                <option value="">Selecione um mês</option>
+                <option value="01">Janeiro</option>
+                <option value="02">Fevereiro</option>
+                <option value="03">Março</option>
+                <option value="04">Abril</option>
+                <option value="05">maio</option>
+                <option value="06">Junho</option>
+                <option value="07">Julho</option>
+                <option value="08">Agosto</option>
+                <option value="09">Setembro</option>
+                <option value="10">Outubro</option>
+                <option value="11">Novembro</option>
+                <option value="12">Dezembro</option>
+              </select>
+            </div>
+            <div className="col-md-3">
+            <label htmlFor="ano" className ="form-label">Mês</label>
+              <select className="form-control" name="ano" id="ano">
+                <option value="">Selecione um ano</option>
+                {
+                  anos.map(ano => {
+                    return <option value={ano}>{ano}</option>
+                  })
+                }
+              </select>
+            </div>
+            <div className="col-md-3">
+              <input type="submit" className="btn btn-lg btn-primary" value="Filtrar"/>
+            </div>
+          </div>
+        </form>
+      <div className="card">
+        <div className="card-header">
+          <h2>Despesas</h2>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="card-body">
+          <ExpensesTable expenses={expenses} />
+        </div>
       </div>
     </main>
   )
 }
+
