5a9326e677319e7a4b39232d009592da26845aed
Johnny <joaogbdesouza@gmail.com>
2024-01-16 20:36:00 -0300

    adicionando validação básica aos campos

--- src/app/nova-despesa/page.tsx
+++ src/app/nova-despesa/page.tsx
@@ -53,28 +53,28 @@ export default async function NovaDespesa() {
       <div className="row">
       <div className="col-md-3">
         <label htmlFor="name">Nome</label>
-        <input name="name" id="name" className="form-control" type="text" />
+        <input name="name" required id="name" className="form-control" type="text" />
       </div>
       <div className="col-md-3">
         <label htmlFor="date">Data</label>
-        <input name="date" id="date" className="form-control" type="date" />
+        <input name="date" id="date" required className="form-control" type="date" />
       </div>
       <div className="col-md-3">
         <label htmlFor="currentInstallment">Parcela atual</label>
-        <input name="currentInstallment" id="currentInstallment" className="form-control" type="number" />
+        <input name="currentInstallment" required id="currentInstallment" className="form-control" type="number" />
       </div>
       <div className="col-md-3">
         <label htmlFor="totalInstallments">Total de parcelas</label>
-        <input name="totalInstallments" id="totalInstallments" className="form-control" type="number" />
+        <input name="totalInstallments" required id="totalInstallments" className="form-control" type="number" />
       </div>
     </div>
     <div className="row">
       <div className="col-md-6">
-        <SelectComponent array={tiposDespesa} label="expenseTypeId" />
+        <SelectComponent array={tiposDespesa} label="Tipo de despesa" inputName="expenseTypeId" required="required" />
       </div>
       <div className="col-md-6">
         <label htmlFor="amount">Valor</label>
-        <input name="amount" id="amount" className="form-control" type="number" />
+        <input name="amount" id="amount" step=".01" className="form-control" type="number" />
       </div>
     </div>
     <div className="row text-center mt-3">
